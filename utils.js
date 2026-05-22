1abe48e1f375b5b636a1f8c456e5f84f7935a50b
bnuchen <bnuchengg@gmail.com>
2026-05-23 07:54:47 +0800

    utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,14 @@
-//
+const Utils = {
+    isTouchScreen: function () {
+        return navigator.maxTouchPoints > 0;
+    },
+    scroll2Pos: function (pos) {
+        scroller.scrollTo({
+            top: pos,
+            behavior: 'smooth'
+        });
+    }
+};
+
+// 一次性暴露
+window.Utils = Utils;
