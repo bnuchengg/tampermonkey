2f59cf99cc7b985c1152084e9a2a37ec2d04ff34
bnuchen <bnuchengg@gmail.com>
2026-05-23 08:04:18 +0800

    utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,22 @@
 const Utils = {
+    resetPos: function () {
+        scroll2Pos(0);
+        isScrollDown = true;
+        contextMenu.replaceChildren(liBottom); // 用参数替换所有子元素?
+    },
+    toggleButton: function () {
+        isScrollDown = !isScrollDown;
+        isScrollDown ? contextMenu.replaceChildren(liBottom) : contextMenu.replaceChildren(liTop);
+    },
+    zoomNext: function (img) {
+        const imgs = Array.from(document.querySelectorAll("img")).filter(img => img.getBoundingClientRect().height >= 150); // 隐藏图片的getBoundingClientRect返回的height为0?
+        const index = Number(imgs.map((item, index) => {
+            item.setAttribute("data-index", index);
+            return item;
+        }).filter(item => item == img)[0]?.getAttribute("data-index"));
+        const next = isScrollDown ? index + 1 : index - 1;
+        imgs[next]?.classList.add("zoomed");
+    },
     isTouchScreen: function () {
         return navigator.maxTouchPoints > 0;
     },
